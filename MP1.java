52b0b1b6bd29d2b5142b17c0d35265501d24bf43
sergio giraldo <sergiorgiraldo@gmail.com>
2015-08-24 21:47:49 -0300

    initial version

---
+++ MP1.java
@@ -0,0 +1,121 @@
+import java.io.File;
+import java.lang.reflect.Array;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.*;
+
+public class MP1 {
+    Random generator;
+    String userName;
+    String inputFileName;
+    String delimiters = " \t,;.?!-:@[](){}_*/";
+    String[] stopWordsArray = {"i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours",
+            "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its",
+            "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that",
+            "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having",
+            "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while",
+            "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before",
+            "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again",
+            "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each",
+            "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than",
+            "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"};
+
+    void initialRandomGenerator(String seed) throws NoSuchAlgorithmException {
+        MessageDigest messageDigest = MessageDigest.getInstance("SHA");
+        messageDigest.update(seed.toLowerCase().trim().getBytes());
+        byte[] seedMD5 = messageDigest.digest();
+
+        long longSeed = 0;
+        for (int i = 0; i < seedMD5.length; i++) {
+            longSeed += ((long) seedMD5[i] & 0xffL) << (8 * i);
+        }
+
+        this.generator = new Random(longSeed);
+    }
+
+    Integer[] getIndexes() throws NoSuchAlgorithmException {
+        Integer n = 10000;
+        Integer number_of_lines = 50000;
+        Integer[] ret = new Integer[n];
+        this.initialRandomGenerator(this.userName);
+        for (int i = 0; i < n; i++) {
+            ret[i] = generator.nextInt(number_of_lines);
+        }
+        return ret;
+    }
+
+    public MP1(String userName, String inputFileName) {
+        this.userName = userName;
+        this.inputFileName = inputFileName;
+    }
+
+    public int indexOf(int[] array, int key) {
+        int returnvalue = -1;
+        for (int i = 0; i < array.length; ++i) {
+            if (key == array[i]) {
+                returnvalue = i;
+                break;
+            }
+        }
+        return returnvalue;
+    }
+
+    public String[] process() throws Exception {
+        String[] ret = new String[20];
+        int index = -1;
+        Integer[] indexes = getIndexes(); 
+
+        ArrayList<String> lines = new ArrayList<String>();
+
+        try(BufferedReader br = new BufferedReader(new FileReader(inputFileName))) {
+
+            for(String line; (line = br.readLine()) != null; ) {
+                index += 1;
+                if (Arrays.asList(indexes).indexOf(index) == -1) continue;
+                lines.add(line);
+            }            
+        }
+
+        SortedMap<String, Integer> tokens = new TreeMap<String, Integer>();
+
+        for (Integer i : indexes) {
+            StringTokenizer st = new StringTokenizer(lines[i], delimiters);
+            while (st.hasMoreTokens() {
+                String str = st.nextToken();
+                if (Arrays.asList(stopWordsArray).indexOf(str) != -1){
+                    if (tokens.containsKey(str))
+                        tokens[key] += 1;
+                    else
+                        tokens.put(str, 1);
+                }
+            }
+        }
+
+        tokens.sort(tokens.entries()); 
+
+        int count = 0;
+        for (Map.Entry<K,V> entry:source.entrySet()) {
+            if (count >= 20) break;
+
+            ret.push(entry.getKey());
+            count++;
+        }
+
+        return ret;
+    }
+
+    public static void main(String[] args) throws Exception {
+        if (args.length < 1){
+            System.out.println("MP1 <User ID>");
+        }
+        else {
+            String userName = args[0];
+            String inputFileName = "./input.txt";
+            MP1 mp = new MP1(userName, inputFileName);
+            String[] topItems = mp.process();
+            for (String item: topItems){
+                System.out.println(item);
+            }
+        }
+    }
+}
